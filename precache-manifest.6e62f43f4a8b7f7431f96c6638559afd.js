b6345deee227e0e811d399069d78c11b42e701b7
supradatripathy789@gmail.com <supradatripathy789@gmail.com>
2021-08-23 18:10:57 +0530

    Updates

---
+++ precache-manifest.6e62f43f4a8b7f7431f96c6638559afd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f659d518cab8be04d60b3716030c0b",
+    "url": "/CodePen-Clone-Using-React/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/CodePen-Clone-Using-React/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "2fa4cd123cda7637c0ef",
+    "url": "/CodePen-Clone-Using-React/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/CodePen-Clone-Using-React/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CodePen-Clone-Using-React/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa4cd123cda7637c0ef",
+    "url": "/CodePen-Clone-Using-React/static/js/main.6f940ff8.chunk.js"
+  },
+  {
+    "revision": "ed8d43418e4fa941905f",
+    "url": "/CodePen-Clone-Using-React/static/js/runtime-main.307f7048.js"
+  }
+]);
